aed7c3d6117e97acccda9d65c7f1e16a8fc00f12
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-09-09 08:23:51 -0500

    Cadenas

---
+++ cadenas.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  String nombre = "Pérez Cuadros Victor Eduardo";
+  print("Buenos dias $nombre");
+  print("Buenos dias ${nombre.toLowerCase()}");
+  print("Buenos dias ${nombre.toUpperCase()}");
+  print("Tu nombre tiene ${nombre.length} letras");
+}
