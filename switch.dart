fda7498389d5c67e7d65fc622772becf3fbb5f03
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-09-15 12:35:27 -0500

    Switch

---
+++ switch.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  String sexo = 'Masculino';
+  switch (sexo) {
+    case 'Femenino':
+      print('Estimada: ');
+      break;
+    case 'Masculino':
+      print('Estimado: ');
+      break;
+    case 'No binario':
+      print('Estimade: ');
+      break;
+    default:
+      print('Hola Mundo');
+      break;
+  }
+}
