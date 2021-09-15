41df22818d1c71d4284eadad7f6b6430d9153c97
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-09-15 11:41:17 -0500

    Booleanos Y Condicionales

---
+++ booleanosycondiciones.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  int edad1 = 15;
+  int edad2 = 18;
+
+  if (edad1 > edad2) {
+    print("Eres mayor");
+  } else {
+    print("Eres menor");
+  }
+
+  // bool mayor = edad1 < edad2;
+
+  // if (true == mayor) {
+  //   print("Eres mayor");
+  // } else {
+  //   print("Eres menor");
+  // }
+
+  // bool aprobado = false;
+  // print(aprobado);
+  // print(mayor);
+}
