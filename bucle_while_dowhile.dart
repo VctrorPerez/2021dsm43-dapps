05d97b3faf85d2146e337233a029c26aaa69de9b
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-09-22 18:13:28 -0500

    While and DoWhile Tarea2

--- bucle_while_dowhile.dart
+++ bucle_while_dowhile.dart
@@ -1,10 +1,14 @@
 void main(List<String> args) {
+  //La sentencia while ejecuta continuamente un bloque
+  //de instrucciones mientras sea verdadera una determinada condición
   int numero = 5;
   int numero2 = 6;
   while (numero <= numero2) {
     print('Funcionando: $numero');
     ++numero;
   }
+  //La instrucción do-while permite repetir una instrucción o una instrucción
+  //compuesta hasta que una expresión especificada sea false
   numero = 15;
   numero2 = 14;
   do {
