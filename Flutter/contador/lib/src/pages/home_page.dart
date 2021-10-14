37cf69976b639217203d6d466ab14254e6f7cb27
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-10-13 21:58:20 -0500

    Contador problema del stateless widget

--- Flutter/contador/lib/src/pages/home_page.dart
+++ Flutter/contador/lib/src/pages/home_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  TextStyle estiloTexto = new TextStyle(fontSize: 25);
+  final estiloTexto = new TextStyle(fontSize: 25);
+
+  final conteo = 10;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,7 @@ class HomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text('Numero de clicks:', style: estiloTexto),
-          Text('0', style: estiloTexto),
+          Text('$conteo', style: estiloTexto),
         ],
       )),
       floatingActionButton: FloatingActionButton(
