2d4b296a094bd6e523939a2403d7608406b7cdeb
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-11-03 17:05:59 -0600

    Optimizacion Codigo

--- Flutter/peliculas/peliculas/lib/providers/movies_provider.dart
+++ Flutter/peliculas/peliculas/lib/providers/movies_provider.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:peliculas/models/models.dart';
+import 'package:peliculas/models/popular_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = '78902f3ade0dfea48b63fec9bd1d54e5';
   String _baseUrl = 'api.themoviedb.org';
   String _language = 'es-ES';
 
+  List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
+  int _popularPage = 0;
+
   MoviesProvider() {
     print('Movies Provider inicializado');
     this.getOnDisplayMovies();
+    this.getPopularMovies();
   }
-  getOnDisplayMovies() async {
-    var url = Uri.https(_baseUrl, '3/movie/now_playing',
-        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+  Future<String> _getJsonData(String endpoint, [int page = 1]) async {
+    var url = Uri.https(_baseUrl, endpoint,
+        {'api_key': _apiKey, 'language': _language, 'page': '$page'});
 
     // Await the http get response, then decode the json-formatted response.
     final response = await http.get(url);
-    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
-    print(nowPlayingResponse.results[0].title);
+    return response.body;
+  }
+
+  getOnDisplayMovies() async {
+    final jsonData = await this._getJsonData('3/movie/now_playing');
+    final nowPlayingResponse = NowPlayingResponse.fromJson(jsonData);
+    onDisplayMovies = nowPlayingResponse.results;
+
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    _popularPage++;
+    final jsonData = await this._getJsonData('3/movie/popular', _popularPage);
+    final popularResponse = PopularResponse.fromJson(jsonData);
+    popularMovies = [...popularMovies, ...popularResponse.results];
+
+    notifyListeners();
   }
 }
--- Flutter/peliculas/peliculas/lib/src/pages/home_page.dart
+++ Flutter/peliculas/peliculas/lib/src/pages/home_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+    print(moviesProvider.onDisplayMovies);
+
     return Scaffold(
         appBar: AppBar(
           title: Text('Peliculas En Cartelera'),
@@ -20,8 +25,11 @@ class HomePage extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              CardSwiper(),
-              MovieSlider(),
+              CardSwiper(movies: moviesProvider.onDisplayMovies),
+              MovieSlider(
+                movies: moviesProvider.popularMovies,
+                title: 'Populares',
+              ),
             ],
           ),
         ));
