0726562325bad4f4b62005ac800d7bc0b5d0b32f
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-11-10 13:55:01 -0600

    Metodo Post Modificado

--- Flutter/Obtener datos de internet/datosinternet/lib/main.dart
+++ Flutter/Obtener datos de internet/datosinternet/lib/main.dart
@@ -1,6 +1,21 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import "dart:convert";
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+
+  if (response.statusCode == 200) {
+    // Si la llamada al servidor fue exitosa, analiza el JSON
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // Si la llamada no fue exitosa, lanza un error.
+    throw Exception('Failed to load post');
+  }
+}
 
 class Post {
   final int userId;
@@ -14,9 +29,50 @@ class Post {
       required this.title,
       required this.body});
 
-  Post.fromJson(Map<String, dynamic> json)
-      : userId = json['userId'],
-        id = json['id'],
-        title = json['title'],
-        body = json['body'];
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+void main() => runApp(MyApp(post: fetchPost()));
+
+class MyApp extends StatelessWidget {
+  final Future<Post> post;
+
+  MyApp({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+            future: post,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // Por defecto, muestra un loading spinner
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
