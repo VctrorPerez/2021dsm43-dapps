297bd6c41963b8ea46156ffada811d204e091d29
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-11-10 14:03:17 -0600

    Creacion Clase Post

--- Flutter/Obtener datos de internet/datos_internet/lib/main.dart
+++ Flutter/Obtener datos de internet/datos_internet/lib/main.dart
@@ -3,3 +3,25 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(
+      {required this.userId,
+      required this.id,
+      required this.title,
+      required this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
