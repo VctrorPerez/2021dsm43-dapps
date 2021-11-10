748d87dd9438cc42570280864be1aaa6c2fbc33f
2021 DSM43 Perez Cuadros Victor Eduardo <al222010016@gmail.com>
2021-11-10 14:03:52 -0600

    Creacion Metodo Post

--- Flutter/Obtener datos de internet/datos_internet/lib/main.dart
+++ Flutter/Obtener datos de internet/datos_internet/lib/main.dart
@@ -25,3 +25,16 @@ class Post {
     );
   }
 }
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+
+  if (response.statusCode == 200) {
+    // Si la llamada al servidor fue exitosa, analiza el JSON
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    // Si la llamada no fue exitosa, lanza un error.
+    throw Exception('Failed to load post');
+  }
+}
